2c25e982b1c32ae6c7d3fc00739f86761a0b46fb
ibrhmkurt <ibrahimkurt@proton.me>
2023-06-16 01:19:29 +0300

    initial  commit

---
+++ 1.js
@@ -0,0 +1,13 @@
+const message = "Hello World";
+const bool = true;
+let num = 42;
+
+num = 5;
+// bool =  false; // gives error
+
+const nonExisting = undefined;
+const empty = null;
+
+//console.log(message, bool, num, nonExisting, empty);
+//document.write(message);
+
