7a8725c37e9fd8428c3de655ed624786d28f9703
ibrhmkurt <ibrahimkurt@protonmail.com>
2024-01-17 04:06:04 +0300

    if conditions

---
+++ Week 1/2.js
@@ -0,0 +1,10 @@
+let a = 50;
+let b = 50;
+
+if (a < b) {
+    console.log("a is less than b");
+} else if (a > b) {
+    console.log("a is greater than b");
+}   else {
+    console.log("a is equal to b");
+}   
