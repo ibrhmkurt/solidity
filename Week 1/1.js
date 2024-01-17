3272eeba55183cf14085aefd6482811c2647abef
ibrhmkurt <ibrahimkurt@protonmail.com>
2024-01-17 03:55:35 +0300

    edited

--- Week 1/1.js
+++ Week 1/1.js
@@ -2,12 +2,17 @@ const message = "Hello World";
 const bool = true;
 let num = 42;
 
+console.log(num);
+
 num = 5;
+
+console.log(num);
+
 // bool =  false; // gives error
 
 const nonExisting = undefined;
 const empty = null;
 
-//console.log(message, bool, num, nonExisting, empty);
-//document.write(message);
+console.log(message, bool, num, nonExisting, empty);
+document.write(message);
 
